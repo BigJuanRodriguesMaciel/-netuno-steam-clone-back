a191635fc778ea97ff86250ecb4cf5bbcf27cb18
HenriqueRomero27 <69323647+HenriqueRomero27@users.noreply.github.com>
2022-09-29 16:49:53 -0300

    :feat:configuration database created

--- src/shared/http/sever.ts
+++ src/shared/http/sever.ts
@@ -1,15 +1,17 @@
-//import { Router } from 'express';
-//import express from 'express'
-//import routes from './routes';
+import 'reflect-metadata';
+import { Router } from 'express';
+import express from 'express'
+import routes from './routes';
+import '@shared/typeorm';
 
-//const app = express();
-//app.use(express.json())
+const app = express();
+app.use(express.json())
 
-//app.use(routes)
+app.use(routes)
 
-//const routes = Router();
-//routes.get('/', (req, res) => {
-//   return res.json("hello world")
-// })
+const routes = Router();
+routes.get('/', (req, res) => {
+   return res.json("hello world")
+ })
 
-// app.listen(3000)
+ app.listen(3000)
