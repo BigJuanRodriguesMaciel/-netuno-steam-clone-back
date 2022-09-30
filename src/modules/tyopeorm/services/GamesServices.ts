398aff1d3f85dfada2f9cbe0fb7b4f6c5cf798f6
isabellymello <isamellogoulart@gmail.com>
2022-09-30 16:30:42 -0300

    :feat: migrations, Entity

---
+++ src/modules/tyopeorm/services/GamesServices.ts
@@ -0,0 +1,7 @@
+import { getCustomRepository } from 'typeorm';
+import ofertas from '../entites/OfertasEspeciais';
+import { OfertasEspeciais1664556807151 } from '@shared/typeorm/migrations/1664556807151-OfertasEspeciais';
+
+class GamesServices {
+  public async execute(): Promise
+}
