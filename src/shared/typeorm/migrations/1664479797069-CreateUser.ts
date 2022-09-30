f6c47f151499ae8583379424de3a720a97f5e255
isabellymello <isamellogoulart@gmail.com>
2022-09-30 14:59:50 -0300

    developer

---
+++ src/shared/typeorm/migrations/1664479797069-CreateUser.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class OfertasEspeciais1664556807151 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: "user",
+        columns: [{
+          name: 'id',
+          type: 'uuid',
+          isPrimary:true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'name_user',
+          type: 'varchar'
+        },
+        {
+          name: 'email',
+          type: 'varchar'
+        },
+        {
+          name: 'password',
+          type: 'varchar'
+        },
+        {
+          name: 'country',
+          type: 'varchar'
+        },
+        {
+          name: 'created_at',
+          type: 'timesamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timesamp',
+          default: 'now()'
+        }
+      ],
+      }),
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('user')
+    }
+
+}
