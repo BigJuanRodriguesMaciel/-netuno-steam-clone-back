10e718718842f6ddc121d6530b0f4d9806a20455
HenriqueRomero27 <luishenriquebuenoromero.com>
2022-09-30 17:29:01 -0300

    :feat: create some user services

---
+++ src/modules/users/services/DeleteUserService.ts
@@ -0,0 +1,24 @@
+import User from '../typeorm/entities/User';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from "@modules/users/typeorm/repositories/UsersRepository";
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+    id: string;
+} 
+
+class DeleteUserService {
+    public async execute({id}: IRequest): Promise<void> {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const user = await usersRepository.findOne(id);
+
+        if(!user) {
+            throw new AppError('User not found')
+        }
+
+        await usersRepository.remove(user);
+    }
+}
+
+export default DeleteUserService;
---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,15 @@
+import User from '../typeorm/entities/User';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from "@modules/users/typeorm/repositories/UsersRepository";
+
+class ListUserService {
+    public async execute(): Promise<User[]> {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const users = usersRepository.find();
+
+        return users;
+    }
+}
+
+export default ListUserService;
